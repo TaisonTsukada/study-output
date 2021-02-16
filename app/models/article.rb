ef321b7c1efbad823ef525d7c655ba8b48214fcd
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-16 18:08:40 +0900

    Active strageダウンロード

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,10 +2,10 @@ class Article < ApplicationRecord
   with_options presence: true do
     validates :title
     validates :text
-    validates :image
     validates :user_id
 
     belongs_to :user
+    has_one_attached :image
 
   end
 end
