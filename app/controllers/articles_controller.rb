a583065f845358fd9e2ed85e8df41c8a3b569c88
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-17 14:29:00 +0900

    searchアクションとルーティングの設定

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def search
+    return nil if params[:keyword] == ""
+    tag = Tag.where(['name LIKE ?', "%#{params[:keyword]}%"] )
+    render json:{ keyword: tag }
+  end
+
   private
   def article_params
     params.require(:articles_tag).permit(:title, :content, :name).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "articles#index"
-  resources :articles, only: [:index, :new, :create]
+  resources :articles, only: [:index, :new, :create] do
+    cllection do
+      get 'search'
+    end
+  end
 end
