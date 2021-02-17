20123ddf0d89b9a892c0619d8e5dfcc3d9504a9c
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-17 15:33:11 +0900

    タグ付け投稿機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "articles#index"
   resources :articles, only: [:index, :new, :create] do
-    cllection do
+    collection do
       get 'search'
     end
   end
