01ea7a949c195f47a863ed2ec0e8224ce6608607
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-17 11:31:05 +0900

    タグを含めたアソシエーションの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,15 @@
 class Article < ApplicationRecord
   has_rich_text :content
+  belongs_to :user
+  has_one_attached :image
+  has_many :article_tags
+  has_many :tags, through: :article_tags
+
   with_options presence: true do
     validates :title
     validates :content
   end
 
-  belongs_to :user
-  has_one_attached :image
-
+  
 
 end
