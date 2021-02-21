6f38399c4c773e0327adf21e97e9b8ca0b5aacff
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-21 16:44:52 +0900

    単体テスト実施

--- spec/factories/articles_tag.rb
+++ spec/factories/articles_tag.rb
@@ -1,13 +1,9 @@
 FactoryBot.define do
   factory :articles_tag do
     title {"世界史の勉強方法"}
-    content {"<div>世界史はインプットとアウトプットの割合が大事です</div>"}
+    content {"世界史はインプットとアウトプットの割合が大事です"}
     name {"世界史"}
 
     association :user
-
-    after(:build) do |article|
-      @article = Article.new(title: title, content: content)
-    end
   end
 end
