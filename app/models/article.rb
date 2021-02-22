ce2fa78f84cbbcd2a1cd5a2efefd627c4a84b979
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-22 10:58:55 +0900

    削除機能実装

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
   has_rich_text :content
   belongs_to :user
   has_one_attached :image
-  has_many :article_tag_relations
-  has_many :tags, through: :article_tag_relations
+  has_many :article_tag_relations, dependent: :destroy
+  has_many :tags, through: :article_tag_relations, dependent: :destroy
 end
