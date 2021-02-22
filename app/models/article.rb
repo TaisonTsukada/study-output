29079f6d287f1f957e31dfb5ec3cb7cbf24dadea
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-22 14:24:36 +0900

    バリデーションの追記

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,12 @@
 class Article < ApplicationRecord
   has_rich_text :content
+  acts_as_taggable
   belongs_to :user
   has_one_attached :image
-  has_many :article_tag_relations, dependent: :destroy
-  has_many :tags, through: :article_tag_relations, dependent: :destroy
+
+  with_options presence: true do
+    validates :user_id
+    validates :content
+    validates :tag_list
+  end
 end
--- app/models/tag.rb
+++
@@ -1,5 +0,0 @@
-class Tag < ApplicationRecord
-  has_many :article_tag_relations
-  has_many :articles, through: :article_tag_relations
-  validates :name, uniqueness: true
-end
