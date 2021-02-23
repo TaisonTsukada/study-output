8a0a1cdfd21908c48b13e410e2c2d32eb32beffb
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-23 12:16:44 +0900

    rubocopの実施

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,18 @@
 class CommentsController < ApplicationController
-
   def index
-    
   end
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.build(comment_params)
     @comment.user_id = current_user.id
-    if @comment.save
-      render :index
-    end
+    render :index if @comment.save
   end
 
   def destroy
     @comment = Comment.find(params[:id])
-    if @comment.destroy
-      render :index
-    end
+    render :index if @comment.destroy
   end
-
 
   def comment_params
     params.require(:comment).permit(:text, :article_id, :user_id)
