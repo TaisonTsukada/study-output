084644cef27104cfa8daec482a5aaae82d993b91
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-25 16:41:47 +0900

    非同期でアラート表示

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,16 @@
 class LikesController < ApplicationController
+  before_action :article_params
   def create
     Like.create(user_id: current_user.id, article_id: params[:id])
-    redirect_to article_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, article_id: params[:id]).destroy
-    redirect_to article_path
+  end
+
+  private
+
+  def article_params
+    @article = Article.find(params[:id])
   end
 end
