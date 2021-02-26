1cba4bd4554be0141e14e0e104bffe14e3842835
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-26 16:02:19 +0900

    ビューファイル 以外の基本的なマイページ機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    #ユーザーの投稿一覧
+    @articles = @user.article.all.order(created_at: :desc).page(params[:page]).per(9)
   end
 end
