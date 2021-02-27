241b93ccb68588421de0b5ebf694d5db0862e37e
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-27 15:34:21 +0900

    dependent:destroyの追記

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,7 +5,6 @@ class RelationshipsController < ApplicationController
     following = current_user.follow(@user)
     if following.save
       flash[:success] = 'ユーザーをフォローしました'
-      redirect_to user_path(@user.id)
     else
       flash.now[:alert] = 'ユーザーのフォローに失敗しました'
       redirect_to user_path(@user.id)
@@ -16,7 +15,6 @@ class RelationshipsController < ApplicationController
     following = current_user.unfollow(@user)
     if following.destroy
       flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to user_path(@user.id)
     else
       flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
       redirect_to user_path(@user.id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,9 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_one_attached :image
 
-  has_many :relationships
+  has_many :relationships, foreign_key: "user_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :user
 
   def liked_by?(article_id)
