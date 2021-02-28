182ab7b7c991414b58df0ab0a5537a9363028e2a
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-28 18:06:05 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   end
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
-  resources :users, only: [:show, :update, :edit]
+  resources :users, only: [:show, :update, :edit] do
+    get :followings, on: :member
+    get :followers, on: :member
+  end
+
   resources :relationships, only: [:create, :destroy]
 end
