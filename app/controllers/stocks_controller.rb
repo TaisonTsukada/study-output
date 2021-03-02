1a783b380419b293d599413de2d86396ba2124d6
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-03-02 14:13:49 +0900

    コメントアウトの削除

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -6,11 +6,8 @@ class StocksController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    # 取得した記事がまだストックされていなければ
     unless @article.stocked?(current_user)
-      # ログインしているユーザーを取得してparamsで渡された記事をストック
       @article.stock(current_user)
-      # ajaxでストックボタンを実装
       respond_to do |format|
         format.html { redirect_to request.referrer || root_url }
         format.js
@@ -20,11 +17,8 @@ class StocksController < ApplicationController
 
   def destroy
     @article = Stock.find(params[:id]).article
-    # 取得した記事が既にストックされていれば
     if @article.stocked?(current_user)
-      # ログインしているユーザーを取得してparamsで渡された記事のストック解除
       @article.unstock(current_user)
-      # ajaxでストックボタンを実装
       respond_to do |format|
         format.html { redirect_to request.referrer || root_url }
         format.js
