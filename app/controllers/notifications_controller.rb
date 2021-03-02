fa1e547b4b7b4ceed5c7daca21918bd6d37047c7
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-03-02 10:46:07 +0900

    ページネーションの実装

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   after_action :false_to_true, only: [:index]
 
   def index
-    @notifications = current_user.passive_notifications.page(params[:page]).per(6)
+    @notifications = current_user.passive_notifications.page(params[:page]).per(20)
   end
 
   private
