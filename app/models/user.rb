e594444e83c91907844fe131305638f009ec83ec
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-03-04 13:47:37 +0900

    アバター機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :articles
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_one_attached :image
+  has_one_attached :avatar
   has_many :stocks, dependent: :destroy
 
   # フォロー機能のアソシエーション
