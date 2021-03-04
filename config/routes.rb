73cb9807e423bf45218b271c32e1896c9dae7c8e
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-03-04 13:56:39 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   root to: "articles#index"
   resources :articles do
     resources :comments, only: [:create, :destroy]
