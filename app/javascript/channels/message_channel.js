7a7ba4b948a13ac883c62481d2c3e4dcc7ff8ec1
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-03-16 21:00:27 +0900

    Action cableをエラーなく実装

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -15,6 +15,7 @@ consumer.subscriptions.create("MessageChannel", {
     `<div class="outgoing_msg" >
       <div class="sent_msg">
         <p>${ data.content.message}</p>
+        <span class="time_date">${data.content.created_at}</span>
       </div>
     </div>`;
     const messages = document.getElementById('messages');
