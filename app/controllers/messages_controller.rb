3b97d1fbdb97427468de54ebc7ab507e34431e52
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-03-16 15:42:39 +0900

    EC2に必要な設定を行った

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,11 +3,18 @@ class MessagesController < ApplicationController
 
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
+      @message = Message.new(message_params)
+      @message.save
+      ActionCable.server.broadcast 'message_channel', content: @message
     else
       flash[:alert] = "メッセージ送信に失敗しました。"
     end
     redirect_to "/rooms/#{@message.room_id}"
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id)
   end
 end
 
