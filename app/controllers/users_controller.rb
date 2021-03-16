3069d5e713f84cb1ed0b55d14dcb4a50cc4f7b66
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-03-16 11:43:16 +0900

    usersコントローラーに追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,25 @@ class UsersController < ApplicationController
     else
       @articles = @user.articles.order(created_at: :desc).page(params[:page]).per(9)
     end
+    
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
+  end
   end
 
   def followings
