1c1e1deb474bda728e3e03d0ac981df27b93aecf
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-03-16 11:32:24 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+  
   root to: "articles#index"
   resources :articles do
     resources :comments, only: [:create, :destroy]
-
   end
   
   post 'like/:id' => 'likes#create', as: 'create_like'
@@ -17,9 +17,12 @@ Rails.application.routes.draw do
     get :followings, on: :member
     get :followers, on: :member
   end
-
   resources :relationships, only: [:create, :destroy]
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
+
   resources :notifications, only: :index
   resources :activities, only: :index
+
   resources :stocks, only: [:index, :create, :destroy]
 end
