5a20525e136c47f79f878fedc88b18f6db0f14d3
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-03-19 11:32:24 +0900

    DM通知機能の修正

--- app/controllers/dmnotifications_controller.rb
+++ app/controllers/dmnotifications_controller.rb
@@ -1,14 +1,5 @@
 class DmnotificationsController < ApplicationController
-  after_action :false_to_true
   def index
-    @dmnotifications = current_user.passive_dmnotifications
   end
 
-  private
-
-  def false_to_true
-    @dmnotifications.where(checked: false).each do |dmnotification|
-      dmnotification.update_attributes(checked: true)
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'dmnotifications/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
       }
