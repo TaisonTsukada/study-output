1090ba90f7ee88ccca54ddd04c1b75a7292423dd
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-03-24 16:35:05 +0900

    action cableのユーザーの振り分けを実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,7 +18,12 @@ class MessagesController < ApplicationController
         # 自分の投稿に対するコメントの場合は、通知済みとする
         dmnotification.checked = true if dmnotification.visitor_id == dmnotification.visited_id
         dmnotification.save if dmnotification.valid?
-        ActionCable.server.broadcast 'message_channel', content: @message
+        if @theid.user.avatar.attached?
+          user_image = Rails.application.routes.url_helpers.rails_blob_path(@theid.user.avatar, only_path: true)
+        else
+          user_image = helpers.asset_url('default_user')
+        end
+        ActionCable.server.broadcast 'message_channel', content: @message, room_id: @room.id, user_image: user_image
       end
     else
       redirect_to "/rooms/#{@message.room_id}"
