a47e922c3ad547035036414630d1d5fb99044b61
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-03-25 13:57:27 +0900

    自動スクロールの実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,7 @@ require("trix")
 require("@rails/actiontext")
 require('./jquery.jscroll.min')
 require('../scroll')
+require('./auto-scroll')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
